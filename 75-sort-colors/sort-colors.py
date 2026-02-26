fc0f8bb272805fec8f90959e7184a1d6a7aa5603
jayabarathi06 <jayabarathijayabalan06@gmail.com>
2026-02-26 22:04:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (21.50%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,6 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        if nums==[]:
+            return []
         nums.sort()
         return nums
         
