ca66f8ea5faa95cb45d1018e0b935d296f8e9450
jayabarathi06 <jayabarathijayabalan06@gmail.com>
2026-03-21 21:26:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (66.74%) - LeetSync

--- 2589-maximum-value-of-a-string-in-an-array/maximum-value-of-a-string-in-an-array.py
+++ 2589-maximum-value-of-a-string-in-an-array/maximum-value-of-a-string-in-an-array.py
@@ -2,7 +2,7 @@ class Solution:
     def maximumValue(self, s: List[str]) -> int:
         maxcount=0
         for i in s:
-            if i.isalpha():
+            if i.isalpha():#dont need
                 maxcount=max(maxcount,len(i))
             elif i.isdigit():
                 maxcount=max(maxcount,int(i))
